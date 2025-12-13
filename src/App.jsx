377b82d5e30a5f7121b9bfbaa843c1d3ed07def8
hamid566-tech <hamid.hashimi566@gmail.com>
2025-12-13 15:02:45 +0430

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Index from './component/Index'
-import NotFound from './component/NotFound'
+// import NotFound from './component/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </>
