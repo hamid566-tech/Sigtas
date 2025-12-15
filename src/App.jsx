6ae2e19e5eae756ce89f0919d4c27eb16d7a9a68
hamid566-tech <hamid.hashimi566@gmail.com>
2025-12-15 09:35:56 +0430

    notfound page uncomand

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import './App.css'
 import Index from './component/Index'
-// import NotFound from './component/NotFound'
+import NotFound from './component/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
