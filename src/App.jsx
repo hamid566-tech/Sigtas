5d957112528f997068b0bd5cc983c9ce1d239cbb
hamid566-tech <hamid.hashimi566@gmail.com>
2025-12-15 09:32:54 +0430

    remove base from hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <HashRouter basename='/Sigtas/'>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
